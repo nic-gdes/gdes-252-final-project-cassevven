5af1721faf99589a33a5d73caf04f6dd06d2766b
cassevven <119555884+cassevven@users.noreply.github.com>
2023-12-07 14:45:46 -0800

    gdes final first push

---
+++ script.js
@@ -0,0 +1,55 @@
+// Get the modal and buttons
+const modal = document.getElementById('modal');
+const openModalBtn = document.getElementById('openModalBtn');
+const closeModalBtn = document.getElementById('closeModalBtn');
+
+// Function to open the modal
+function openModal() {
+    modal.style.display = 'block';
+}
+
+// Function to close the modal
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+// Event listeners for the buttons
+openModalBtn.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', closeModal);
+
+// Close the modal if the user clicks outside the modal content
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        closeModal();
+    }
+});
+
+
+//calculator conversion 
+function convert(conversionType) {
+    const inputValue = parseFloat(document.getElementById('inputValue').value);
+    const resultField = document.getElementById('result');
+
+    switch (conversionType) {
+        case 'Fahrenheit to Celsius':
+            resultField.value = ((inputValue - 32) * 5 / 9).toFixed(2) + ' °C';
+            break;
+        case 'Feet to Miles':
+            resultField.value = (inputValue / 5280).toFixed(6) + ' Miles';
+            break;
+        case 'Inches to Centimeters':
+            resultField.value = (inputValue * 2.54).toFixed(2) + ' cm';
+            break;
+        default:
+            resultField.value = 'Invalid conversion';
+    }
+}
+
+// dark mode and light mode switch button 
+document.addEventListener('DOMContentLoaded', function () {
+    const modeSwitch = document.getElementById('modeSwitch');
+    
+    modeSwitch.addEventListener('change', function () {
+        document.body.classList.toggle('dark-mode');
+    });
+});
